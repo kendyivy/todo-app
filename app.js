a1cbe6fc68f6ac644ed96531b2c9be243919c5c6
Student <Student>
2018-03-28 16:35:29 +0300

    add text

--- app.js
+++ app.js
@@ -1,5 +1,36 @@
-console.log("app loaded")
+const form = document.querySelector("form")
+const taskInput = document.querySelector("input[name=taskName]")
 
-const div = document.querySelector(".container")
+const dateInput = document.querySelector("input[name=taskDate]")
+const ul = document.querySelector("ul")
 
-console.log(div)
+
+form.onsubmit = function(e){
+	e.preventDefault()
+     
+
+     let taskTitle = taskInput.value
+     let taskDate = dateInput.value
+
+
+
+
+     const li = document.createElement("li")
+     li.textContent = taskTitle
+     li.classList.add("list-group-item")
+
+
+     const span = document.createElement("span")
+     span.textContent = taskDate
+     span.classList.add("badge")
+     span.classList.add("badge-primary")
+     span.classList.add("badge-pill")
+
+     li.appendChild(span)
+
+
+
+     ul.appendChild(li)
+
+}
+
